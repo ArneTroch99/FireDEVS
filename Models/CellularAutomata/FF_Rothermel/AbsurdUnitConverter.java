d47e2a3bc098b39fb75a0fac59b2e8c0b73b5e8b
Siemen Herremans <siemen.herremans@student.uantwerpen.be>
2021-04-14 16:59:29 +0200

    Added t/ac to lb/ft²

--- Models/CellularAutomata/FF_Rothermel/AbsurdUnitConverter.java
+++ Models/CellularAutomata/FF_Rothermel/AbsurdUnitConverter.java
@@ -2,8 +2,12 @@ package CellularAutomata.FF_Rothermel;
 
 public class AbsurdUnitConverter {
 
-    private static double ch_h_to_ft_min(double ch_h){
+    // Convert chains per hour to feet per minute
+    public static double ch_h_to_ft_min(double ch_h){
         return ch_h * 1.1;
     }
+
+    // Convert tons per acre to pounds per feet²
+    public static double t_ac_to_lb_ftsqrd(double t_ac){return t_ac * 0.0459136823;}
 }
 
