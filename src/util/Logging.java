d736c3ae21218a08517a221d7fe439fed29f726c
Arne Troch <arne.troch@student.uantwerpen.be>
2021-04-21 15:01:54 +0200

    - Added personal log levels to logger

--- src/util/Logging.java
+++ src/util/Logging.java
@@ -18,15 +18,15 @@ public class Logging
      * Logging levels that may be chosen from for the level variable below.
      * More may be added as necessary.
      */
-    static public final int full = 100, none = 0, errorsOnly = 10,
-        inputViolations = 20;
+    static public final int full = 100, none = 0, errorsOnly = 10, inputViolations = 20,
+                            info = 5, debug = 4, warning = 3, fatal = 2;
 
     /**
      * The current logging level used throughout the GenDevs code.  This
      * is meant to control what kinds (if any) of logging messages get
      * displayed on stdout.
      */
-    static public final int level = errorsOnly;
+    static public final int level = inputViolations;
 
     /**
      * Writes the given message to stdout.
